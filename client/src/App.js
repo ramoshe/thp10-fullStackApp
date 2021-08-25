184a119665054d3bdf8c5d046e3dbc5b67b26ae6
Rachel Sherard <rsherard@mac.com>
2021-08-25 05:50:11 -0400

    test API connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ courses, setCourses ] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/courses', { method: 'GET', mode: 'cors' })
+      .then(response => response.json())
+      .then(data => setCourses(data.courses));
+  }, []);
+
+  const titles = courses.map(course => course.title);
+  let titleKey = 0;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header></header>
+      <main>
+        <div className="wrap main--grid">
+          {titles.map(title => (
+            <a 
+              className="course--module course--link" 
+              href="course-detail.html" 
+              key={titleKey+=1}>
+                <h2 className="course--label">Course</h2>
+                <h3 className="course--title">{title}</h3>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
 
