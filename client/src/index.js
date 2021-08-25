af3cbadc2a0373424e25fc4ef32fbe38549ad000
Rachel Sherard <rsherard@mac.com>
2021-08-25 05:46:06 -0400

    relocate and import css files

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './styles/reset.css';
+import './styles/global.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
