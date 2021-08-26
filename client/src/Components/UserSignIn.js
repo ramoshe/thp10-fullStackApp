08cd5458ee6c48e55754e2b4265fe1a63811661a
Rachel Sherard <rsherard@mac.com>
2021-08-26 05:31:54 -0400

    fix syntax in User components

--- client/src/Components/UserSignIn.js
+++ client/src/Components/UserSignIn.js
@@ -14,10 +14,10 @@ const UserSignIn = () => {
             <div className="form--centered">
                 <h2>Sign In</h2>
                 <form>
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="" />
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value="" />
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" />
                     <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick={handleSubmit}>Cancel</button>
                 </form>
                 <p>Don't have a user account? Click here to <Link to="/signup">sign up</Link>!</p>
--- client/src/Components/UserSignUp.js
+++ client/src/Components/UserSignUp.js
@@ -14,14 +14,14 @@ const UserSignUp = () => {
             <div className="form--centered">
                 <h2>Sign Up</h2>
                 <form>
-                    <label for="firstName">First Name</label>
-                    <input id="firstName" name="firstName" type="text" value="" />
-                    <label for="lastName">Last Name</label>
-                    <input id="lastName" name="lastName" type="text" value="" />
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="" />
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value="" />
+                    <label htmlFor="firstName">First Name</label>
+                    <input id="firstName" name="firstName" type="text" />
+                    <label htmlFor="lastName">Last Name</label>
+                    <input id="lastName" name="lastName" type="text" />
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" />
                     <button className="button" type="submit">Sign Up</button><button className="button button-secondary" onClick={handleSubmit}>Cancel</button>
                 </form>
                 <p>Already have a user account? Click here to <Link to="/signin">sign in</Link>!</p>
